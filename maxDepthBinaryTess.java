62bea97bce79816a0b851d1a1baef8361407d406
rean.wen <reaniwen@gmail.com>
2014-11-29 16:30:44 -0500

    min & max Depth of Binary Tree

---
+++ maxDepthBinaryTess.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null){
+            return maxDepth(root.right)+1;
+        }
+        else if(root.right == null){
+            return maxDepth(root.left)+1;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+//         if(root.left == null && root.right == null){
+//             return 1;
+//         }
+// /**         
+//  *          else if(root.left == null){
+//  *          return maxDepth(root.right)+1;     
+//  *      }
+//         else if(root.right == null){
+//             return maxDepth(root.left)+1;
+//         }
+//         **/
+//         else{
+//             int le = maxDepth(root.left);
+//             int ri = maxDepth(root.right);
+//             int dep = le>ri ? le+1 : ri+1;
+//             return dep;
+//         }
+    }
+}
